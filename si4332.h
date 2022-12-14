a0e17ff7e27a1a61d8a9e7ab3cf9d78c7a3d2a3a
Selim Gullulu <59433968+selimg76@users.noreply.github.com>
2022-12-15 01:04:10 +0300

    Create si4332.h

---
+++ si4332.h
@@ -0,0 +1,16 @@
+/*
+ * si4332.h
+ *
+ *  Created on: 11 Oct 2022
+ *      Author: Selim Gullulu
+ */
+
+#ifndef SI4332_H_
+#define SI4332_H_
+
+#define W_REGISTER       (0b10000000)  // Write Registers
+#define R_REGISTER       (0b00000000)  // Read Registers
+
+
+
+#endif /* SI4332_H_ */
